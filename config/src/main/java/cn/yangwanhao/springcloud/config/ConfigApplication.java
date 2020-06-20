1752d37f0890d66cc28cd9ee180965a8382290ad
杨万浩 <yangwanhao163@163.com>
2020-06-20 21:20:08 +0800

    init config

---
+++ config/src/main/java/cn/yangwanhao/springcloud/config/ConfigApplication.java
@@ -0,0 +1,22 @@
+package cn.yangwanhao.springcloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 启动类
+ *
+ * @author yangwh@yunrong.cn
+ * @version V2.1
+ * @since 2020/6/20 12:52
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
