1758a3539eb5cc398dd4698eb34fd51a52f57877
杨万浩 <yangwanhao163@163.com>
2020-06-21 15:31:44 +0800

    init zuul

---
+++ zuul/src/main/java/cn/yangwanhao/springcloud/zuul/ZuulApplication.java
@@ -0,0 +1,16 @@
+package cn.yangwanhao.springcloud.zuul;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 启动类
+ *
+ * @author 杨万浩
+ * @version V1.0.0
+ * @since 2020/6/20 21:22
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulApplication {
+}
