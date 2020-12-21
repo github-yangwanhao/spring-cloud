56709c115a4900a5ddd71e67988a21a150230e0f
杨万浩 <yangwanhao163@163.com>
2020-12-21 14:28:10 +0800

    版本修改 & maven修改

--- zuul/src/main/java/cn/yangwanhao/springcloud/zuul/ZuulApplication.java
+++ zuul/src/main/java/cn/yangwanhao/springcloud/zuul/ZuulApplication.java
@@ -1,5 +1,6 @@
 package cn.yangwanhao.springcloud.zuul;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
@@ -13,4 +14,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
 }
